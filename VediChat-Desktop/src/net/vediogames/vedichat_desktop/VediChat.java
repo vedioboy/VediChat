7acb6464743f74d316e12f9b8bf77be11d78d38e
vedioboy <vedioboy@vediogames.net>
2015-03-02 20:58:47 -0500

    First commit. Just to put it out there

---
+++ VediChat-Desktop/src/net/vediogames/vedichat_desktop/VediChat.java
@@ -0,0 +1,9 @@
+package net.vediogames.vedichat_desktop;
+
+public class VediChat {
+
+	public static void main(String[] args) {
+		System.out.println("Nothing here right now.");
+	}
+
+}
---
+++ VediChat-Server/src/net/vediogames/vedichat_server/VediChatServer.java
@@ -0,0 +1,13 @@
+package net.vediogames.vedichat_server;
+
+public class VediChatServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This current version of VediChat does not use dedicated servers yet." +
+				" They will be added soon in a future update.");
+	}
+
+}
