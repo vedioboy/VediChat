8f633d885f22fd6fd0fff29ff58babb7b20806cb
vedioboy <vedioboy@vediogames.net>
2015-03-03 07:43:30 -0500

    Changed desktop version so that it can be used in swing designer.

--- VediChat-Desktop/src/net/vediogames/vedichat_desktop/VediChat.java
+++
@@ -1,9 +0,0 @@
-package net.vediogames.vedichat_desktop;
-
-public class VediChat {
-
-	public static void main(String[] args) {
-		System.out.println("Nothing here right now.");
-	}
-
-}
---
+++ VediChat-Desktop/src/net/vediogames/vedichat_desktop/VediChatMain.java
@@ -0,0 +1,43 @@
+package net.vediogames.vedichat_desktop;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class VediChatMain {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VediChatMain window = new VediChatMain();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VediChatMain() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
